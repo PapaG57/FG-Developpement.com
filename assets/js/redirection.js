6fb216ccc992d80522027e2e5cf567a218d4462c
Florent GERARD <florentgerard@sfr.fr>
2024-03-04 19:06:46 +0100

    fin code page accueil

---
+++ assets/js/redirection.js
@@ -0,0 +1,9 @@
+// redirection vers la page suivante une fois la vidéo lue et le spinner affiché
+
+document.getElementById('myVideo').addEventListener('ended', function() {
+    // Lorsque la vidéo est terminée
+    document.getElementById('spinner').style.display = 'block'; // Afficher le spinner
+    setTimeout(function() {
+        window.location.href = 'index.html'; // Rediriger vers la page suivante après un délai
+    }, 3000); // Délai de 3 secondes (2000 millisecondes) pour le spinner
+});
