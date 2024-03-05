2ed9c4682f6d29455cc6fca2ecb481b9022f7d60
Florent GERARD <florentgerard@sfr.fr>
2024-03-05 12:49:22 +0100

    ajout de la section about et revalorisation code autres pages

--- assets/js/redirection.js
+++ assets/js/redirection.js
@@ -5,5 +5,5 @@ document.getElementById('myVideo').addEventListener('ended', function() {
     document.getElementById('spinner').style.display = 'block'; // Afficher le spinner
     setTimeout(function() {
         window.location.href = 'accueil.html'; // Rediriger vers la page suivante après un délai
-    }, 3000); // Délai de 3 secondes (2000 millisecondes) pour le spinner
+    }, 2000); // Délai de 2 secondes (2000 millisecondes) pour le spinner
 });
